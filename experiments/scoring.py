9841fe2c460c5ae446a8cd45c993be619307da03
sean <smatthews39@gatech.edu>
2019-03-18 20:49:18 -0700

    modifying scoring average

--- experiments/scoring.py
+++ experiments/scoring.py
@@ -10,7 +10,7 @@ def balanced_accuracy(truth, pred):
 
 def f1_accuracy(truth, pred):
     wts = compute_sample_weight('balanced', truth)
-    return f1_score(truth, pred, average="binary", sample_weight=wts)
+    return f1_score(truth, pred, average="micro", sample_weight=wts)
 
 
 scorer = make_scorer(balanced_accuracy)
